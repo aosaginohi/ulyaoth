cb8b6c8fd6217548ee94df8955052a8399c519e4
Sjir Bagmeijer <sbagmeijer@ulyaoth.asia>
2014-08-24 14:33:50 +0200

    bash script
    autobuild

---
+++ Repository/ulyaoth-nginx/build-ulyaoth-nginx-modsecurity.sh
@@ -0,0 +1,40 @@
+useradd ulyaoth
+yum install -y pcre pcre-devel libxml2 libxml2-devel curl curl-devel httpd-devel yajl-devel lua-devel lua-static
+mkdir -p /etc/nginx/modules
+cd /etc/nginx/modules
+wget https://www.modsecurity.org/tarball/2.8.0/modsecurity-2.8.0.tar.gz
+tar xvf modsecurity-2.8.0.tar.gz
+mv modsecurity-2.8.0 modsecurity
+rm -rf modsecurity-2.8.0.tar.gz
+cd modsecurity
+./autogen.sh
+./configure --enable-standalone-module
+make
+cd /root
+rpmdev-setuptree
+cd /root/rpmbuild/SOURCES
+wget http://nginx.org/download/nginx-1.6.1.tar.gz
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/logrotate
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/modsecurity.conf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.conf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.init
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.service
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.suse.init
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.sysconf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.upgrade.sh
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.vh.default-modsecurity.conf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.vh.default.conf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.vh.example_ssl.conf
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/nginx.vh.passenger.conf
+cd /root/rpmbuild/SPECS
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx-modsecurity.spec
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx-passenger.spec
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx.spec
+cd /home/ulyaoth/
+chown -R ulyaoth:ulyaoth /etc/nginx/
+mv /root/rpmbuild /home/ulyaoth/
+chown -R ulyaoth:ulyaoth /home/ulyaoth/rpmbuild
+cd /home/ulyaoth/rpmbuild/SPECS
+yum-builddep -y ulyaoth-nginx-modsecurity.spec
+su ulyaoth -c rpmbuild -bb ulyaoth-nginx-modsecurity.spec
+cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
