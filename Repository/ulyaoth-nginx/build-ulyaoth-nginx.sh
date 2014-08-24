cb6dca58f5c50f6ba4bc3b06042f98788bbbb5c6
Sjir Bagmeijer <sbagmeijer@ulyaoth.asia>
2014-08-24 14:55:48 +0200

    bash and fix

--- Repository/ulyaoth-nginx/build-ulyaoth-nginx.sh
+++ Repository/ulyaoth-nginx/build-ulyaoth-nginx.sh
@@ -1,14 +1,6 @@
 useradd ulyaoth
 cd /root
 rpmdev-setuptree
-mkdir -p /etc/nginx/modules
-cd /etc/nginx/modules
-wget http://s3.amazonaws.com/phusion-passenger/releases/passenger-4.0.49.tar.gz
-tar xvf passenger-4.0.49.tar.gz
-mv passenger-4.0.49 passenger
-rm -rf /etc/nginx/modules/passenger/packaging
-tar cvf passenger-f20.tar.gz passenger
-mv passenger-f20.tar.gz /root/rpmbuild/SOURCES/
 cd /root/rpmbuild/SOURCES
 wget http://nginx.org/download/nginx-1.6.1.tar.gz
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SOURCES/logrotate
@@ -27,21 +19,9 @@ cd /root/rpmbuild/SPECS
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx-modsecurity.spec
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx-passenger.spec
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx.spec
-cd /home/ulyaoth/
-chown -R ulyaoth:ulyaoth /etc/nginx/
 mv /root/rpmbuild /home/ulyaoth/
 chown -R ulyaoth:ulyaoth /home/ulyaoth/rpmbuild
 cd /home/ulyaoth/rpmbuild/SPECS
 yum-builddep -y ulyaoth-nginx-passenger.spec
-su ulyaoth -c "rpmbuild -bb ulyaoth-nginx-passenger.spec"
-rm -rf /home/ulyaoth/rpmbuild/BUILD/*
-rm -rf /home/ulyaoth/rpmbuild/BUILDROOT/*
-rm -rf /home/ulyaoth/rpmbuild/RPMS/*
-rm -rf /home/ulyaoth/rpmbuild/SOURCES/passenger-f20.tar.gz
-cd /etc/nginx/modules
-tar cvf passenger-f20.tar.gz passenger
-mv passenger-f20.tar.gz /home/ulyaoth/rpmbuild/SOURCES/
-chown -R ulyaoth:ulyaoth /home/ulyaoth/rpmbuild
-cd /home/ulyaoth/rpmbuild/SPECS
 su ulyaoth -c "rpmbuild -bb ulyaoth-nginx-passenger.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
