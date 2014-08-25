24ace5fa2186e63a74a64921c21e61bc7cf5115c
Sjir Bagmeijer <sbagmeijer@ulyaoth.asia>
2014-08-25 18:38:32 +0200

    Changed to use "ulyaoth" user.

--- Repository/ulyaoth/build-ulyaoth.sh
+++ Repository/ulyaoth/build-ulyaoth.sh
@@ -1,14 +1,11 @@
-cd /root
-rpmdev-setuptree
-cd /root/rpmbuild/SOURCES
-wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/RPM-GPG-KEY-ulyaoth
-wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-fedora.repo
-wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-rhel.repo
-cd /root/rpmbuild/SPECS
-wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SPECS/ulyaoth.spec
-mv /root/rpmbuild /home/ulyaoth/
-chown -R ulyaoth:ulyaoth /home/ulyaoth/rpmbuild
+useradd ulyaoth
+su ulyaoth -c "rpmdev-setuptree"
+cd /home/ulyaoth/rpmbuild/SOURCES
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/RPM-GPG-KEY-ulyaoth"
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-fedora.repo"
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-rhel.repo"
 cd /home/ulyaoth/rpmbuild/SPECS
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SPECS/ulyaoth.spec"
 su ulyaoth -c "rpmbuild -bb ulyaoth.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
-rm -rf /home/ulyaoth/rpmbuild
+su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild"
