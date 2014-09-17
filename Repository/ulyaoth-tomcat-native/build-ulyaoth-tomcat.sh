9527472fef0908d56f189837483bcd991686958a
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-09-17 11:57:10 +0200

    small fix

--- Repository/ulyaoth-tomcat-native/build-ulyaoth-tomcat.sh
+++ Repository/ulyaoth-tomcat-native/build-ulyaoth-tomcat.sh
@@ -4,7 +4,7 @@ su ulyaoth -c "rpmdev-setuptree"
 cd /home/ulyaoth/rpmbuild/SOURCES/
 su ulyaoth -c "wget http://apache.mirrors.spacedump.net/tomcat/tomcat-connectors/native/1.1.31/source/tomcat-native-1.1.31-src.tar.gz"
 cd /home/ulyaoth/rpmbuild/SPECS/
-su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat/SPECS/ulyaoth-tomcat.spec"
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat-native/SPECS/ulyaoth-tomcat-native.spec"
 yum-builddep -y ulyaoth-tomcat-native.spec
 su ulyaoth -c "rpmbuild -bb ulyaoth-tomcat-native.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
