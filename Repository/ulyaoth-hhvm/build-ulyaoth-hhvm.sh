7c3cdb8a45049494f72dbc5c961d7244b2887c19
Sjir Bagmeijer <sbagmeijer@ulyaoth.asia>
2014-09-21 10:00:42 +0200

    Update build-ulyaoth-hhvm.sh

--- Repository/ulyaoth-hhvm/build-ulyaoth-hhvm.sh
+++ Repository/ulyaoth-hhvm/build-ulyaoth-hhvm.sh
@@ -9,9 +9,6 @@ wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/
 wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/g/glog-devel-0.3.3-3.fc20.x86_64.rpm
 wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/l/libmcrypt-2.5.8-14.fc20.x86_64.rpm
 wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/l/libmcrypt-devel-2.5.8-14.fc20.x86_64.rpm
-wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/l/libzip-0.11.1-3.fc20.x86_64.rpm
-wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/l/libzip-devel-0.11.1-3.fc20.x86_64.rpm
-wget http://ftp.acc.umu.se/mirror/fedora/linux/releases/20/Everything/x86_64/os/Packages/u/uw-imap-devel-2007f-5.fc20.x86_64.rpm
 yum -y install uw-imap-devel-2007f-5.fc20.x86_64.rpm oniguruma-5.9.5-1.fc20.x86_64.rpm oniguruma-devel-5.9.5-1.fc20.x86_64.rpm libmcrypt-devel-2.5.8-14.fc20.x86_64.rpm libmcrypt-2.5.8-14.fc20.x86_64.rpm glog-devel-0.3.3-3.fc20.x86_64.rpm glog-0.3.3-3.fc20.x86_64.rpm libzip-0.11.1-3.fc20.x86_64.rpm libzip-devel-0.11.1-3.fc20.x86_64.rpm
 rm -rf /home/ulyaoth/oniguruma-5.9.5-1.fc20.x86_64.rpm oniguruma-devel-5.9.5-1.fc20.x86_64.rpm libmcrypt-devel-2.5.8-14.fc20.x86_64.rpm libmcrypt-2.5.8-14.fc20.x86_64.rpm glog-devel-0.3.3-3.fc20.x86_64.rpm glog-0.3.3-3.fc20.x86_64.rpm libzip-0.11.1-3.fc20.x86_64.rpm libzip-devel-0.11.1-3.fc20.x86_64.rpm
 fi
