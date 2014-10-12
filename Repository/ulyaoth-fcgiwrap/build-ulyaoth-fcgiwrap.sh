51986bad52c51bbeb28a0c57d601124d24ad87eb
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-10-12 19:38:56 +0200

    ulyaoth-fcgiwrap rpm

---
+++ Repository/ulyaoth-fcgiwrap/build-ulyaoth-fcgiwrap.sh
@@ -0,0 +1,15 @@
+useradd ulyaoth
+cd /home/ulyaoth/
+su ulyaoth -c "rpmdev-setuptree"
+su ulyaoth -c "git clone https://github.com/gnosek/fcgiwrap"
+su ulyaoth -c "sed -i 's/http/fcgiwrap/g' /home/ulyaoth/fcgiwrap/systemd/fcgiwrap.service"
+su ulyaoth -c "rm -rf /home/ulyaoth/fcgiwrap/.git/"
+su ulyaoth -c "tar cvf  fcgiwrap"
+su ulyaoth -c "mv fcgiwrap.tar.gz /home/ulyaoth/rpmbuild/SOURCES/"
+cd /home/ulyaoth/rpmbuild/SPECS/
+yum-builddep -y /home/ulyaoth/rpmbuild/SPECS/ulyaoth-fcgiwrap.spec
+su ulyaoth -c "rpmbuild -bb ulyaoth-fcgiwrap.spec"
+cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
+rm -rf /home/ulyaoth/rpmbuild/
+rm -rf /etc/nginx
+rm -rf /root/build-ulyaoth-fcgiwrap.sh
