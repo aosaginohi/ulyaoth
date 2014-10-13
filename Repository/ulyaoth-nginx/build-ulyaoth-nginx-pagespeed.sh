d8a97a19a09868b14f4bcf4d354df6f6dc7fccef
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-10-13 17:30:47 +0200

    Changes Issue #824
    https://github.com/pagespeed/ngx_pagespeed/issues/824#issuecomment-58857935

--- Repository/ulyaoth-nginx/build-ulyaoth-nginx-pagespeed.sh
+++ Repository/ulyaoth-nginx/build-ulyaoth-nginx-pagespeed.sh
@@ -12,8 +12,8 @@ su ulyaoth -c "mv psol/ /etc/nginx/modules/pagespeed/"
 su ulyaoth -c "rm -rf /etc/nginx/modules/pagespeed/psol/lib/Debug/linux/ia32"
 su ulyaoth -c "rm -rf /etc/nginx/modules/pagespeed/psol/lib/Release/linux/ia32"
 su ulyaoth -c "rm -rf 1.9.32.1.tar.gz ngx_pagespeed-release-1.9.32.1-beta release-1.9.32.1-beta.zip"
-cd /etc/nginx/modules/
-su ulyaoth -c "tar cvf pagespeed.tar.gz pagespeed"
+cd /etc/nginx/modules/pagespeed/
+su ulyaoth -c "tar cvf pagespeed.tar.gz scripts/ test/"
 su ulyaoth -c "mv pagespeed.tar.gz /home/ulyaoth/rpmbuild/SOURCES/"
 cd /home/ulyaoth/rpmbuild/SOURCES/
 su ulyaoth -c "wget http://nginx.org/download/nginx-1.6.2.tar.gz"
@@ -29,15 +29,6 @@ su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/
 cd /home/ulyaoth/rpmbuild/SPECS
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/SPECS/ulyaoth-nginx-pagespeed.spec"
 yum-builddep -y /home/ulyaoth/rpmbuild/SPECS/ulyaoth-nginx-pagespeed.spec
-su ulyaoth -c "rpmbuild -bb ulyaoth-nginx-pagespeed.spec"
-su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild/BUILD/*"
-su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild/BUILDROOT/*"
-su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild/RPMS/*"
-su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild/SOURCES/pagespeed.tar.gz"
-cd /etc/nginx/modules/
-su ulyaoth -c "tar cvf pagespeed.tar.gz pagespeed"
-su ulyaoth -c "mv pagespeed.tar.gz /home/ulyaoth/rpmbuild/SOURCES/"
-cd /home/ulyaoth/rpmbuild/SPECS/
 su ulyaoth -c "rpmbuild -bb ulyaoth-nginx-pagespeed.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 rm -rf /home/ulyaoth/rpmbuild/
