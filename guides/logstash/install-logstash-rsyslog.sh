de26bdd4d24641384fd1acc1a91dc92db411138e
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-10-21 20:02:41 +0200

    Updated to work with ES 1.4 and Kibana 4

--- guides/logstash/install-logstash-rsyslog.sh
+++ guides/logstash/install-logstash-rsyslog.sh
@@ -1,11 +1,10 @@
 yum install -y https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.4.2-1_2c0f5a1.noarch.rpm
-yum install -y https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.noarch.rpm
-yum install -y nginx rsyslog tar wget policycoreutils-python zip
+yum install -y https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.0.Beta1.noarch.rpm
+yum install -y nginx rsyslog tar wget policycoreutils-python zip java-1.8.0-openjdk.
 cd /etc/logstash/conf.d
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/rsyslog/logstash.conf
 chown logstash:logstash logstash.conf
 mkdir -p /var/log/nginx/kibana
-mkdir -p /usr/share/nginx/kibana/public
 mkdir -p /etc/nginx/sites-available
 mkdir -p /etc/nginx/sites-enabled
 rm -rf /etc/nginx/nginx.conf
@@ -14,18 +13,14 @@ wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash
 cd /etc/nginx/sites-available/
 wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/nginx/vhost/kibana.conf
 ln -s /etc/nginx/sites-available/kibana.conf /etc/nginx/sites-enabled/kibana.conf
-cd /usr/share/nginx/kibana/public
-wget https://download.elasticsearch.org/kibana/kibana/kibana-latest.tar.gz
-tar xzfv kibana-latest.tar.gz
-mv kibana-latest/* .
-rm -rf kibana-latest.tar.gz
-rm -rf kibana-latest
-sed -i 's/default.json/ulyaoth.json/' config.js
-cd /usr/share/nginx/kibana/public/app/dashboards
-wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/kibana/dashboard/ulyaoth.json
+cd /opt
+wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-BETA1.1.tar.gz
+tar xzfv kibana-4.0.0-BETA1.1.tar.gz
+mv kibana-4.0.0-BETA1.1 kibana
+rm -rf kibana-4.0.0-BETA1.1.tar.gz
 useradd -s /sbin/nologin kibana
-chown -R kibana:nginx /usr/share/nginx/kibana/
-chcon -R -t httpd_sys_content_t /usr/share/nginx/kibana/public/
+chown -R kibana:kibana /opt/kibana/
+setsebool httpd_can_network_connect true 
 semanage port -a -t http_port_t -p tcp 9200
 firewall-cmd --zone=public --add-service=http
 firewall-cmd --zone=public --add-service=https
