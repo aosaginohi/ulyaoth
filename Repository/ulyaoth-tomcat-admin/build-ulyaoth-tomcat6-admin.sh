ea1fd287cb0ad89124b360754cdd583e096739bf
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-11-17 10:21:46 +0100

    small script fix

--- Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
+++ Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
@@ -5,8 +5,7 @@ cd /home/ulyaoth/rpmbuild/SOURCES/
 su ulyaoth -c "wget http://apache.mirrors.spacedump.net/tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41.tar.gz"
 cd /home/ulyaoth/rpmbuild/SPECS/
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat-admin/SPECS/ulyaoth-tomcat6-admin.spec"
-yum-builddep -y ulyaoth-tomcat6.spec
 su ulyaoth -c "rpmbuild -bb ulyaoth-tomcat6-admin.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild"
-rm -rf /root/build-ulyaoth-tomcat6.sh
+rm -rf /root/build-ulyaoth-tomcat6-admin.sh
