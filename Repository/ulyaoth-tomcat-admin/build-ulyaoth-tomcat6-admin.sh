abfbd4bff2f50176eb81cf57e3f1c0fb26a56a73
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-11-17 10:03:04 +0100

    change spec file

--- Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
+++ Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
@@ -4,7 +4,7 @@ su ulyaoth -c "rpmdev-setuptree"
 cd /home/ulyaoth/rpmbuild/SOURCES/
 su ulyaoth -c "wget http://apache.mirrors.spacedump.net/tomcat/tomcat-6/v6.0.41/bin/apache-tomcat-6.0.41.tar.gz"
 cd /home/ulyaoth/rpmbuild/SPECS/
-su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat/SPECS/ulyaoth-tomcat6.spec"
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat-admin/SPECS/ulyaoth-tomcat6-admin.spec"
 yum-builddep -y ulyaoth-tomcat6.spec
 su ulyaoth -c "rpmbuild -bb ulyaoth-tomcat6.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
