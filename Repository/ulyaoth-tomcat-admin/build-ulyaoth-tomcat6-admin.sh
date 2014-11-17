59aea8cac003248ba2be8fc20e195afab02192c2
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2014-11-17 10:08:44 +0100

    another spec fix

--- Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
+++ Repository/ulyaoth-tomcat-admin/build-ulyaoth-tomcat6-admin.sh
@@ -6,7 +6,7 @@ su ulyaoth -c "wget http://apache.mirrors.spacedump.net/tomcat/tomcat-6/v6.0.41/
 cd /home/ulyaoth/rpmbuild/SPECS/
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-tomcat-admin/SPECS/ulyaoth-tomcat6-admin.spec"
 yum-builddep -y ulyaoth-tomcat6.spec
-su ulyaoth -c "rpmbuild -bb ulyaoth-tomcat6.spec"
+su ulyaoth -c "rpmbuild -bb ulyaoth-tomcat6-admin.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild"
 rm -rf /root/build-ulyaoth-tomcat6.sh
