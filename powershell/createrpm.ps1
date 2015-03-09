8f60656b84cac4eb0b2f7c2835e8c347f460d3dc
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-09 14:15:13 +0100

    Added some fixes.

--- powershell/createrpm.ps1
+++ powershell/createrpm.ps1
@@ -1,3 +1,4 @@
+
  <# Require the package name as parameter to build #>
  param (
     [string]$package = $(throw "-package is required.")
@@ -5,10 +6,14 @@
  
 "CHECK 0: a valid parameter was provide ($package)"
 
+ <# Set all required variables. #>
+ $userAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::PowerShell
+
 <# check if the ulyaoth directory exist and if not then create it #>
 if(!(Test-Path -Path c:\ulyaoth))
   {
    new-item c:\ulyaoth -itemtype directory
+   "The directory 'c:\ulyaoth' was created."
   }
 else
   {
@@ -18,7 +23,8 @@ else
   <# check if the plink application exist and if not then download it #>
   if(!(Test-Path -Path c:\ulyaoth\plink.exe))
   {
-   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/plink.exe -Method Get -OutFile c:\ulyaoth\putty.exe -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
+   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/plink.exe -Method Get -OutFile c:\ulyaoth\plink.exe -UserAgent $userAgent
+   "The program plink was downloaded."
   }
 else
   {
@@ -28,7 +34,8 @@ else
   <# check if the psftp application exist and if not then download it #>
   if(!(Test-Path -Path c:\ulyaoth\psftp.exe))
   {
-   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/psftp.exe -Method Get -OutFile c:\ulyaoth\psftp.exe -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer)
+   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/psftp.exe -Method Get -OutFile c:\ulyaoth\psftp.exe
+   "The program psftp was downloaded."
   }
 else
   {
