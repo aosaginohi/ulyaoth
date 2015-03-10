fbc6cdce71a86dcb96eac4cbaa12b99ea7ccb98e
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-10 18:34:15 +0100

    fix

--- powershell/createrpm.ps1
+++ powershell/createrpm.ps1
@@ -9,7 +9,7 @@ param (
  
 <# Set all required variables. #>
 $PackageArray = @("ulyaoth-nginx", "ulyaoth-nginx-pagespeed", "ulyaoth-nginx-modsecurity", "ulyaoth-nginx-naxsi-masterbuild", "ulyaoth-nginx-passenger4", "ulyaoth-nginx-passenger5", "ulyaoth-kibana4", "ulyaoth-tomcat6", "ulyaoth-tomcat7", "ulyaoth-tomcat8", "ulyaoth-tomcat6-admin", "ulyaoth-tomcat7-admin", "ulyaoth-tomcat8-admin", "ulyaoth-tomcat6-docs", "ulyaoth-tomcat7-docs", "ulyaoth-tomcat8-docs", "ulyaoth-tomcat6-examples", "ulyaoth-tomcat7-examples", "ulyaoth-tomcat8-examples", "ulyaoth-tomcat-native", "ulyaoth-logstah-forwarder-masterbuild", "ulyaoth-fcgiwrap", "ulyaoth-hhvm")
-$MachineArray = @{ 'cc41ec2f-7aae-47d9-a910-70b02b71d535' = '192.168.1.72'; 'b' = 2; 'c' = 3}
+$MachineArray = @{ 'cc41ec2f-7aae-47d9-a910-70b02b71d535' = '192.168.1.91'; 'b' = 2; 'c' = 3}
 
 <# Set the correct build variable based on package input #>
 if ($PackageArray -contains $package)
@@ -26,20 +26,20 @@ Else
  
  
 <# check if the ulyaoth directory exist and if not then create it #>
-if(!(Test-Path -Path c:\ulyaoth))
+if(!(Test-Path -Path c:\ulyaoth\createrpm))
   {
-   new-item c:\ulyaoth -itemtype directory
-   "The directory 'c:\ulyaoth' was created."
+   new-item c:\ulyaoth\createrpm -itemtype directory
+   "The directory 'c:\ulyaoth\createrpm' was created."
   }
 else
   {
-   "CHECK 1: ulyaoth directory already exists"
+   "CHECK 1: c:\ulyaoth\createrpm  directory already exists"
   }
   
   <# check if the plink application exist and if not then download it #>
-  if(!(Test-Path -Path c:\ulyaoth\plink.exe))
+  if(!(Test-Path -Path c:\ulyaoth\createrpm\plink.exe))
   {
-   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/plink.exe -Method Get -OutFile c:\ulyaoth\plink.exe
+   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/plink.exe -Method Get -OutFile c:\ulyaoth\createrpm\plink.exe
    "The program plink was downloaded."
   }
 else
@@ -48,9 +48,9 @@ else
   }
   
   <# check if the psftp application exist and if not then download it #>
-  if(!(Test-Path -Path c:\ulyaoth\psftp.exe))
+  if(!(Test-Path -Path c:\ulyaoth\createrpm\psftp.exe))
   {
-   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/psftp.exe -Method Get -OutFile c:\ulyaoth\psftp.exe
+   Invoke-WebRequest -uri https://trash.ulyaoth.net/trash/exe/putty/0.63/psftp.exe -Method Get -OutFile c:\ulyaoth\createrpm\psftp.exe
    "The program psftp was downloaded."
   }
 else
@@ -62,25 +62,25 @@ ForEach ($buildbox in $MachineArray.GetEnumerator()) {
 {
 <# Create the virtual machine #>
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" clonevm $buildbox.Name --name buildmachine --mode all --options keepallmacs --register
-"Creating the virtual machine: $buildbox.Name"
+"Creating the virtual machine"
 
 <# If we build ulyaoth-hhvm then give the server more ram and cpus #>
 If ($package -Match "ulyaoth-hhvm")
 {
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm $buildbox.Name --vram 8192 --cpus 4
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm buildmachine --vram 8192 --cpus 4
 "We are building $package so increasing Memory to 8GB and cpus to 4."
 }
 
 <# Start the virtual machine #>
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm $buildbox.Name
-"Starting the virtual machine: $buildbox.Name"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm buildmachine
+"Starting the virtual machine"
   
 <# Sleep for 60 seconds so machine can boot #>
 Start-Sleep -Seconds 60
 "Sleeping 60 seconds while waiting for the Virtual Machine to boot."
 
 <# ssh into the machine and start the rpm build process #>
-echo y | C:\ulyaoth\plink.exe -ssh root@$buildbox.Value -pw $password "$build"
+echo y | c:\ulyaoth\createrpm\plink.exe -ssh root@$buildbox.Value -pw $password "$build"
 "Running the build script on: $buildbox.Name"
 
 <# Sleep for 5 minutes by default to build the package or 2 hours if building hhvm #>
@@ -96,16 +96,16 @@ Start-Sleep -Seconds 300
 }
 
 <# Poweroff the virtual machine #>
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm $buildbox.Name poweroff
-"Stopping the virtual machine: $buildbox.Name"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm buildmachine poweroff
+"Stopping the virtual machine"
 
 <# Sleep for 30 seconds so machine can power off #>
 Start-Sleep -Seconds 30
 "Sleeping 30 seconds while waiting for the Virtual Machine to power off."
 
 <# Delete the virtual machine #>
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" unregistervm --delete $buildbox.Name
-"Deleting the virtual machine: $buildbox.Name"
+& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" unregistervm --delete buildmachine
+"Deleting the virtual machine"
 
 <# Sleep for 10 seconds before looping again #>
 Start-Sleep -Seconds 10
