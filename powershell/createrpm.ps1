b7b1cd8b32a4eb8a58303b259b56ddc94dd1b040
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-10 11:27:38 +0100

    Changing stuff around.

--- powershell/createrpm.ps1
+++ powershell/createrpm.ps1
@@ -3,15 +3,47 @@
  param (
     [string]$package = $(throw "-package is required."),
     [string]$password = $(throw "-password is required."),
-	[string]$repouser = $(throw "-repouser is required."),
-	[string]$repopass = $(throw "-repopass is required.")
+    [string]$repouser = $(throw "-repouser is required."),
+    [string]$repopass = $(throw "-repopass is required.")
  )
  
-"CHECK 0: a valid parameter was provide ($package) and a password was provided."
-
  <# Set all required variables. #>
  $userAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer
 
+<# Set the correct build variable based on package input #>
+ If ($package -eq ulyaoth-nginx)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx.sh ; chmod +x build-ulyaoth-nginx.sh ; ./build-ulyaoth-nginx.sh"
+ }
+ ElseIf ($package -eq ulyaoth-nginx-pagespeed)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-pagespeed.sh ; chmod +x build-ulyaoth-nginx-pagespeed.sh ; ./build-ulyaoth-nginx-pagespeed.sh"
+ }
+ ElseIf ($package -eq ulyaoth-nginx-modsecurity)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-modsecurity.sh ; chmod +x build-ulyaoth-nginx-modsecurity.sh ; ./build-ulyaoth-nginx-modsecurity.sh"
+ }
+ ElseIf ($package -eq ulyaoth-nginx-naxsi-masterbuild)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-naxsi-masterbuild.sh ; chmod +x build-ulyaoth-nginx-naxsi-masterbuild.sh ; ./build-ulyaoth-nginx-naxsi-masterbuild.sh"
+ }
+ ElseIf ($package -eq ulyaoth-nginx-passenger4)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-passenger4.sh ; chmod +x build-ulyaoth-nginx-passenger4.sh ; ./build-ulyaoth-nginx-passenger4.sh"
+ }
+  ElseIf ($package -eq ulyaoth-nginx-passenger5)
+ {
+   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-passenger5.sh ; chmod +x build-ulyaoth-nginx-passenger5.sh ; ./build-ulyaoth-nginx-passenger5.sh"
+ }
+ Else
+{
+ "Only a supported Ulyaoth repository package can be used as input."
+ exit
+} 
+ 
+"CHECK 0: a valid parameter was provide ($package)."
+ 
+ 
 <# check if the ulyaoth directory exist and if not then create it #>
 if(!(Test-Path -Path c:\ulyaoth))
   {
@@ -44,40 +76,10 @@ else
   {
    "CHECK 3: psftp program is already available"
   }
-  
- <# Set the correct build variable based on package input #>
- If ($package -eq ulyaoth-nginx)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx.sh ; chmod +x build-ulyaoth-nginx.sh ; ./build-ulyaoth-nginx.sh"
- }
- ElseIf ($package -eq ulyaoth-nginx-pagespeed)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-pagespeed.sh ; chmod +x build-ulyaoth-nginx-pagespeed.sh ; ./build-ulyaoth-nginx-pagespeed.sh"
- }
- ElseIf ($package -eq ulyaoth-nginx-modsecurity)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-modsecurity.sh ; chmod +x build-ulyaoth-nginx-modsecurity.sh ; ./build-ulyaoth-nginx-modsecurity.sh"
- }
- ElseIf ($package -eq ulyaoth-nginx-naxsi-masterbuild)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-naxsi-masterbuild.sh ; chmod +x build-ulyaoth-nginx-naxsi-masterbuild.sh ; ./build-ulyaoth-nginx-naxsi-masterbuild.sh"
- }
- ElseIf ($package -eq ulyaoth-nginx-passenger4)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-passenger4.sh ; chmod +x build-ulyaoth-nginx-passenger4.sh ; ./build-ulyaoth-nginx-passenger4.sh"
- }
-  ElseIf ($package -eq ulyaoth-nginx-passenger5)
- {
-   $build = "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx/build-ulyaoth-nginx-passenger5.sh ; chmod +x build-ulyaoth-nginx-passenger5.sh ; ./build-ulyaoth-nginx-passenger5.sh"
- }
- Else
-{
- "Only a supported Ulyaoth repository package can be used as input."
- exit
-}
 
- 
- 
+
+
+  
 <# Create the Fedora 19 64bit virtual machine #>
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" clonevm cc41ec2f-7aae-47d9-a910-70b02b71d535 --name f19rpmx64 --mode all --options keepallmacs --register
 
@@ -88,7 +90,7 @@ else
 Start-Sleep -Seconds 60
 "Sleeping 60 seconds while waiting for the Virtual Machine to boot."
 
-<# ssh into the machine and test it works #>
+<# ssh into the machine and start the rpm build process #>
 echo y | C:\ulyaoth\plink.exe -ssh root@192.168.1.72 -pw $password '$build'
 
 <# Sleep for 5 minutes to build the package #>
