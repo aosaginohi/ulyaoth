984c5220b08f4f3fd6c08477da7535e079daa834
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-11 01:29:20 +0100

    more fixes.

--- Repository/ulyaoth/build-ulyaoth.sh
+++ Repository/ulyaoth/build-ulyaoth.sh
@@ -2,18 +2,14 @@ useradd ulyaoth
 su ulyaoth -c "rpmdev-setuptree"
 cd /home/ulyaoth/rpmbuild/SOURCES
 
-if grep -q -i "release 7" /etc/redhat-release
+if grep -q -i "Red Hat" /etc/redhat-release
 then
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-rhel.repo -O ulyaoth.repo"
-else
-su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-centos.repo -O ulyaoth.repo"
-fi
-
-if grep -q -i "release 6" /etc/redhat-release
+elif grep -q -i "CentOS" /etc/centos-release
 then
-su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-rhel.repo"
-else
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/ulyaoth-centos.repo -O ulyaoth.repo"
+else
+echo yeah Fedora!
 fi
 
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SOURCES/RPM-GPG-KEY-ulyaoth"
@@ -22,5 +18,11 @@ cd /home/ulyaoth/rpmbuild/SPECS
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth/SPECS/ulyaoth.spec"
 su ulyaoth -c "rpmbuild -bb ulyaoth.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
-su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild"
-rm -rf /root/build-ulyaoth.sh
+
+
+
+[root@localhost ~]# ./test.sh -b ulyaoth -u sjir -p test -r repo.ulyaoth.net
+Package: ulyaoth
+User: sjir
+Password: test
+Repo: repo.ulyaoth.net
