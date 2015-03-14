2539ee6e76ca110ea5650e03f44d624173095f86
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-14 13:39:00 +0100

    fix fix fix

--- Repository/ulyaoth-logstash-forwarder/build-ulyaoth-logstash-forwarder-masterbuild.sh
+++ Repository/ulyaoth-logstash-forwarder/build-ulyaoth-logstash-forwarder-masterbuild.sh
@@ -1,10 +1,26 @@
 arch="$(uname -m)"
 buildarch="$(uname -m)"
 
+if [ "$arch" == "i686" ]
+then
+arch="i386"
+fi
+
+if grep -q -i "release 6" /etc/redhat-release
+then
+yum install -y http://ftp.acc.umu.se/mirror/fedora/epel/6/$arch/epel-release-6-8.noarch.rpm
+elif grep -q -i "release 6" /etc/centos-release
+then
+yum install -y http://ftp.acc.umu.se/mirror/fedora/epel/6/$arch/epel-release-6-8.noarch.rpm
+else
+echo yeah Fedora!
+fi
+
+
 useradd ulyaoth
 cd /home/ulyaoth/
 su ulyaoth -c "rpmdev-setuptree"
-yum install -y go
+yum install -y go golang
 su ulyaoth -c "git clone git://github.com/elasticsearch/logstash-forwarder.git"
 su ulyaoth -c "cd /home/ulyaoth/logstash-forwarder/ && go build"
 su ulyaoth -c "mv /home/ulyaoth/logstash-forwarder/logstash-forwarder /home/ulyaoth/rpmbuild/SOURCES/"
