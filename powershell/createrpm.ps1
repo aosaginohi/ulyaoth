5d65416de5e9d71581471a9740207a4cbff2d7e5
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-14 08:53:44 +0100

    less sleeps to speed things up

--- powershell/createrpm.ps1
+++ powershell/createrpm.ps1
@@ -77,9 +77,9 @@ If ($package -Match "ulyaoth-hhvm")
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm buildmachine
 "Starting the virtual machine"
   
-<# Sleep for 30 seconds so machine can boot #>
-"Sleeping 30 seconds while waiting for the Virtual Machine to boot."
-Start-Sleep -Seconds 30
+<# Sleep for 25 seconds so machine can boot #>
+"Sleeping 25 seconds while waiting for the Virtual Machine to boot."
+Start-Sleep -Seconds 25
 
 <# ssh into the machine and start the rpm build process #>
 "Running the build script"
@@ -89,15 +89,15 @@ echo y | c:\ulyaoth\createrpm\plink.exe -ssh -l root $buildbox.Value -pw $passwo
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm buildmachine poweroff
 "Stopping the virtual machine"
 
-<# Sleep for 15 seconds so machine can power off #>
-"Sleeping 15 seconds while waiting for the Virtual Machine to power off."
-Start-Sleep -Seconds 15
+<# Sleep for 10 seconds so machine can power off #>
+"Sleeping 10 seconds while waiting for the Virtual Machine to power off."
+Start-Sleep -Seconds 10
 
 <# Delete the virtual machine #>
 & "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" unregistervm --delete buildmachine
 "Deleting the virtual machine"
 
-<# Sleep for 10 seconds before looping again #>
-"Sleeping 10 seconds just to make sure the delete operation is finished."
-Start-Sleep -Seconds 10
+<# Sleep for 5 seconds before looping again #>
+"Sleeping 5 seconds just to make sure the delete operation is finished."
+Start-Sleep -Seconds 5
 }
