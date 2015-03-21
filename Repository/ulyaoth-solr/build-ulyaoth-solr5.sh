3b02b84b851b9d3317a87970d927a54270a5906c
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-03-21 10:34:09 +0100

    missed wget

--- Repository/ulyaoth-solr/build-ulyaoth-solr5.sh
+++ Repository/ulyaoth-solr/build-ulyaoth-solr5.sh
@@ -6,7 +6,7 @@ cd /home/ulyaoth
 yum install -y ant
 su ulyaoth -c "rpmdev-setuptree"
 
-su ulyaoth -c "http://apache.mirrors.spacedump.net/lucene/solr/5.0.0/solr-5.0.0.tgz"
+su ulyaoth -c "wget http://apache.mirrors.spacedump.net/lucene/solr/5.0.0/solr-5.0.0.tgz"
 su ulyaoth -c "tar xvf solr-$version.tgz"
 su ulyaoth -c "mv solr-$version solr"
 rm -rf /home/ulyaoth/solr/examples
