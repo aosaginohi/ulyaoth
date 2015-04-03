b75e65112d2405d3a87e4465737d7b19e42a71df
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-04-03 13:42:49 +0200

    fix build to have specific version per os.

--- Repository/ulyaoth-nginx-pagespeed/build-ulyaoth-nginx-pagespeed-selinux.sh
+++ Repository/ulyaoth-nginx-pagespeed/build-ulyaoth-nginx-pagespeed-selinux.sh
@@ -6,15 +6,15 @@ then
 arch="i386"
 fi
 
+yum install -y policycoreutils-python
 useradd ulyaoth
-cd /home/ulyaoth/
-chown -R ulyaoth:ulyaoth /etc/nginx
 su ulyaoth -c "rpmdev-setuptree"
-cd /home/ulyaoth/rpmbuild/SOURCES/
-su ulyaoth -c "wget https://github.com/sbagmeijer/ulyaoth/raw/master/Repository/ulyaoth-nginx-pagespeed/SOURCES/ulyaoth-nginx-pagespeed.pp"
+cd /home/ulyaoth/
+su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx-pagespeed/Selinux/ulyaoth-nginx-pagespeed.txt"
+su ulyaoth -c "audit2allow -M ulyaoth-nginx-pagespeed < ulyaoth-nginx-pagespeed.txt"
+su ulyaoth -c "mv ulyaoth-nginx-pagespeed.pp /home/ulyaoth/rpmbuild/SOURCES/"
 cd /home/ulyaoth/rpmbuild/SPECS
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-nginx-pagespeed/SPECS/ulyaoth-nginx-pagespeed-selinux.spec"
-
 if [ "$arch" != "x86_64" ]
 then
 sed -i '/BuildArch: x86_64/c\BuildArch: '"$buildarch"'' ulyaoth-nginx-pagespeed-selinux.spec
@@ -26,5 +26,5 @@ cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/i386/* /root/
 rm -rf /home/ulyaoth/rpmbuild/
-rm -rf /etc/nginx
+rm -rf /home/ulyaoth/ulyaoth-*
 rm -rf /root/build-ulyaoth-nginx-pagespeed-selinux.sh
