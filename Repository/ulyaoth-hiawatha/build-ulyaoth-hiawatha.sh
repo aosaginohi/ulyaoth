674405ae8049fdb1d9a386d2f98906d64694adcf
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-06-09 17:24:59 +0200

    Used wrong build file.

--- Repository/ulyaoth-hiawatha/build-ulyaoth-hiawatha.sh
+++ Repository/ulyaoth-hiawatha/build-ulyaoth-hiawatha.sh
@@ -7,6 +7,73 @@ su ulyaoth -c "rpmdev-setuptree"
 cd /home/ulyaoth/rpmbuild/SPECS
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-hiawatha/SPECS/ulyaoth-hiawatha.spec"
 
+if grep -q -i "release 19" /etc/fedora-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/i686/ulyaoth-1.0.5-1.fc19.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/x86_64/ulyaoth-1.0.5-1.fc19.x86_64.rpm
+  fi
+elif grep -q -i "release 20" /etc/fedora-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/i686/ulyaoth-1.0.5-1.fc20.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/x86_64/ulyaoth-1.0.5-1.fc20.x86_64.rpm
+  fi
+elif grep -q -i "release 21" /etc/fedora-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/i686/ulyaoth-1.0.5-1.fc21.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/Fedora/x86_64/ulyaoth-1.0.5-1.fc21.x86_64.rpm
+  fi
+elif grep -q -i "release 22" /etc/fedora-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    dnf install https://trash.ulyaoth.net/trash/rpm/Fedora/i686/ulyaoth-1.0.5-1.fc22.i686.rpm
+  else
+    dnf install https://trash.ulyaoth.net/trash/rpm/Fedora/x86_64/ulyaoth-1.0.5-1.fc22.x86_64.rpm
+  fi
+elif grep -q -i "rhel" /etc/ulyaoth && grep -q -i "release 6" /etc/redhat-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/rhel/i686/ulyaoth-1.0.5-1.el6.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/rhel/x86_64/ulyaoth-1.0.5-1.el6.x86_64.rpm
+  fi  
+elif grep -q -i "rhel" /etc/ulyaoth && grep -q -i "release 7" /etc/redhat-release
+then
+  yum install https://trash.ulyaoth.net/trash/rpm/rhel/x86_64/ulyaoth-1.0.5-1.el7.x86_64.rpm
+elif grep -q -i "CentOS" /etc/ulyaoth && grep -q -i "release 6" /etc/centos-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/CentOS/i686/ulyaoth-1.0.5-1.el6.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/CentOS/x86_64/ulyaoth-1.0.5-1.el6.x86_64.rpm
+  fi
+elif grep -q -i "CentOS" /etc/ulyaoth && grep -q -i "release 7" /etc/redhat-release
+then
+  yum install https://trash.ulyaoth.net/trash/rpm/CentOS/x86_64/ulyaoth-1.0.5-1.el7.centos.x86_64.rpm
+elif grep -q -i "OracleLinux" /etc/ulyaoth && grep -q -i "release 6" /etc/oracle-release
+then
+  if [ "$arch" != "x86_64" ]
+  then
+    yum install https://trash.ulyaoth.net/trash/rpm/OracleLinux/i686/ulyaoth-1.0.5-1.el6.i686.rpm
+  else
+    yum install https://trash.ulyaoth.net/trash/rpm/OracleLinux/x86_64/ulyaoth-1.0.5-1.el6.x86_64.rpm
+  fi
+elif grep -q -i "OracleLinux" /etc/ulyaoth && grep -q -i "release 7" /etc/oracle-release
+then
+  yum install https://trash.ulyaoth.net/trash/rpm/OracleLinux/x86_64/ulyaoth-1.0.5-1.el7.x86_64.rpm
+fi
+
 if [ "$arch" != "x86_64" ]
 then
 sed -i '/BuildArch: x86_64/c\BuildArch: '"$buildarch"'' ulyaoth-hiawatha.spec
