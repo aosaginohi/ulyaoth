3c8ebd8b429975886baaf70d8caf340924376ee6
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-07-08 10:52:34 +0200

    Small update.

--- guides/logstash/install-logstash-forwarder.sh
+++ guides/logstash/install-logstash-forwarder.sh
@@ -2,7 +2,7 @@ sudo rpm --import http://packages.elasticsearch.org/GPG-KEY-elasticsearch
 cd /etc/yum.repos.d/
 sudo wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/repository/logstash.repo
 sudo wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/repository/elasticsearch.repo
-sudo yum install -y http://trash.ulyaoth.net/trash/rpm/ulyaoth-1.0.0-1.fc21.x86_64.rpm
+sudo yum install -y http://trash.ulyaoth.net/trash/rpm/ulyaoth-1.0.6-1.fc22.x86_64.rpm
 sudo yum install -y ulyaoth-nginx ulyaoth-kibana ulyaoth-logstash-forwarder java elasticsearch logstash rsyslog tar wget policycoreutils-python zip
 sudo systemctl daemon-reload
 cd /etc/logstash/conf.d
@@ -13,7 +13,7 @@ sudo chown nginx:adm /var/log/nginx
 sudo wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/nginx/vhost/kibana4.conf -O /etc/nginx/sites-available/kibana.conf
 sudo ln -s /etc/nginx/sites-available/kibana.conf /etc/nginx/sites-enabled/kibana.conf
 cd /opt/logstash-forwarder/ssl
-sudo openssl req -x509 -subj '/CN=*.ulyaoth.net/' -nodes -newkey rsa:4096 -keyout logstash-forwarder.key -out logstash-forwarder.crt && chown lforward:lforward *
+sudo openssl req -x509 -subj '/CN=*.ulyaoth.net/' -nodes -newkey rsa:4096 -keyout logstash-forwarder.key -out logstash-forwarder.crt && chown logstash-forwarder:logstash-forwarder *
 sudo semanage port -a -t http_port_t -p tcp 9200
 sudo semanage port -a -t http_port_t -p tcp 5601
 firewall-cmd --permanent --zone=FedoraServer --add-service=http
