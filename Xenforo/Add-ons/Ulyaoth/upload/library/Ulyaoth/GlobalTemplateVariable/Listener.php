0389be088bf23411b006e1514fd7f818773c8b36
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-07-27 08:14:55 +0200

    Updating.

--- Xenforo/Add-ons/Ulyaoth/upload/library/Ulyaoth/GlobalTemplateVariable/Listener.php
+++ Xenforo/Add-ons/Ulyaoth/upload/library/Ulyaoth/GlobalTemplateVariable/Listener.php
@@ -1,8 +1,16 @@
 <?php
 class Ulyaoth_GlobalTemplateVariable_Listener
 {
-    public static function template_create(&$templateName, array &$ulyaoth, XenForo_Template_Abstract $template)
-    {
-        $ulyaoth['sjir'] = "Hello";
-    }
+
+public static function template_create(&$templateName, array &$params, XenForo_Template_Abstract $template)
+{
+  $db = XenForo_Application::get('db');
+  $latestThreads = $db->fetchAll("SELECT * FROM xf_thread AS thread
+                                  JOIN xf_post AS post
+                                      ON thread.first_post_id = post.post_id
+                                  ORDER BY thread.post_date DESC
+                                  LIMIT 30");
+  $params['latestThreads'] = $latestThreads;
+}
+
 }
