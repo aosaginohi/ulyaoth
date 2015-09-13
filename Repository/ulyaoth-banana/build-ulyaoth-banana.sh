070f1abc30100c5842f80ed4085cb7655d41d133
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-09-13 12:59:07 +0200

    reorder build.

--- Repository/ulyaoth-banana/build-ulyaoth-banana.sh
+++ Repository/ulyaoth-banana/build-ulyaoth-banana.sh
@@ -5,12 +5,6 @@ useradd ulyaoth
 cd /home/ulyaoth
 
 su ulyaoth -c "rpmdev-setuptree"
-su ulyaoth -c "wget https://github.com/LucidWorks/banana/archive/v'"$bananaversion"'.tar.gz"
-su ulyaoth -c "tar xvzf v'"$bananaversion"'.tar.gz"
-su ulyaoth -c "mkdir -p /home/ulyaoth/banana-'"$bananaversion"'/build"
-cd /home/ulyaoth/banana-$bananaversion
-su ulyaoth -c "ant"
-mv /home/ulyaoth/banana-$bananaversion/build/banana-0.war /home/ulyaoth/rpmbuild/SOURCES/banana.war
 
 cd /home/ulyaoth/rpmbuild/SPECS/
 su ulyaoth -c "wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/Repository/ulyaoth-banana/SPECS/ulyaoth-banana.spec"
@@ -28,6 +22,16 @@ yum-builddep -y ulyaoth-banana.spec
 fi
 
 su ulyaoth -c "spectool ulyaoth-banana.spec -g -R"
+
+cd /home/ulyaoth
+su ulyaoth -c "wget https://github.com/LucidWorks/banana/archive/v'"$bananaversion"'.tar.gz"
+su ulyaoth -c "tar xvzf v'"$bananaversion"'.tar.gz"
+su ulyaoth -c "mkdir -p /home/ulyaoth/banana-'"$bananaversion"'/build"
+cd /home/ulyaoth/banana-$bananaversion
+su ulyaoth -c "ant"
+mv /home/ulyaoth/banana-$bananaversion/build/banana-0.war /home/ulyaoth/rpmbuild/SOURCES/banana.war
+cd /home/ulyaoth/rpmbuild/SPECS/
+
 su ulyaoth -c "rpmbuild -bb ulyaoth-banana.spec"
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
