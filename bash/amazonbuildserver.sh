78cb4266b3bfedab4d65272b9cfdd141746d1330
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-11-04 19:45:26 +0100

    fix for ulyaoth 1.0.10.

--- bash/amazonbuildserver.sh
+++ bash/amazonbuildserver.sh
@@ -3,5 +3,5 @@ echo 'amazon' | sudo tee --append /etc/ulyaoth > /dev/null
 sudo useradd -G wheel ulyaoth
 sudo yum groupinstall "Development Tools" -y
 sudo yum install rpmdevtools rpm-build perl-macros rpm-sign yum-utils curl wget zip tar system-rpm-config mlocate -y
-sudo yum install https://downloads.ulyaoth.net/rpm/amazon/x86_64/ulyaoth-1.0.9-1.amzn1.x86_64.rpm -y
+sudo yum install https://downloads.ulyaoth.net/rpm/amazon/x86_64/ulyaoth-1.0.10-1.amzn1.x86_64.rpm -y
 reboot
