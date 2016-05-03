94e6b631106456286c2168ae7ca1ea018bbbd46f
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-05-03 22:02:00 +0200

    fix scripts.

--- amazon/powershell/ulyaoth-repository-to-s3-scientificlinux.ps1
+++ amazon/powershell/ulyaoth-repository-to-s3-scientificlinux.ps1
@@ -2,24 +2,24 @@
 #Delete the old repodata folder.
 Remove-Item S:\scientificlinux\6\i386\debug\repodata -recurse
 Remove-Item S:\scientificlinux\6\i386\os\repodata -recurse
-Remove-Item S:\scientificlinux\6\i386\os\source\repodata -recurse
+Remove-Item S:\scientificlinux\6\i386\source\repodata -recurse
 Remove-Item S:\scientificlinux\6\x86_64\debug\repodata -recurse
 Remove-Item S:\scientificlinux\6\x86_64\os\repodata -recurse
-Remove-Item S:\scientificlinux\6\x86_64\os\source\repodata -recurse
+Remove-Item S:\scientificlinux\6\x86_64\source\repodata -recurse
 Remove-Item S:\scientificlinux\7\x86_64\debug\repodata -recurse
 Remove-Item S:\scientificlinux\7\x86_64\os\repodata -recurse
-Remove-Item S:\scientificlinux\7\x86_64\os\source\repodata -recurse
+Remove-Item S:\scientificlinux\7\x86_64\source\repodata -recurse
 
 #Delete the old repoview folder.
 Remove-Item S:\scientificlinux\6\i386\debug\repoview -recurse
 Remove-Item S:\scientificlinux\6\i386\os\repoview -recurse
-Remove-Item S:\scientificlinux\6\i386\os\source\repoview -recurse
+Remove-Item S:\scientificlinux\6\i386\source\repoview -recurse
 Remove-Item S:\scientificlinux\6\x86_64\debug\repoview -recurse
 Remove-Item S:\scientificlinux\6\x86_64\os\repoview -recurse
-Remove-Item S:\scientificlinux\6\x86_64\os\source\repoview -recurse
+Remove-Item S:\scientificlinux\6\x86_64\source\repoview -recurse
 Remove-Item S:\scientificlinux\7\x86_64\debug\repoview -recurse
 Remove-Item S:\scientificlinux\7\x86_64\os\repoview -recurse
-Remove-Item S:\scientificlinux\7\x86_64\os\source\repoview -recurse
+Remove-Item S:\scientificlinux\7\x86_64\source\repoview -recurse
 
 #Copy the new repodata folder.
 Copy-Item R:\Repository\RPMs\scientificlinux\6\i386\debug\repodata S:\scientificlinux\6\i386\debug -recurse
