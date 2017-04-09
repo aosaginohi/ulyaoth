1d43c0f81f1d4a70f18812eea6cf4b6926a5e516
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2017-04-09 17:47:15 +0200

    fixed url

--- bash/amazonbuildserver.sh
+++ bash/amazonbuildserver.sh
@@ -5,5 +5,5 @@ echo 'amazonlinux' | sudo tee --append /etc/ulyaoth > /dev/null
 sudo useradd -G wheel ulyaoth
 sudo yum groupinstall "Development Tools" -y
 sudo yum install rpmdevtools rpm-build perl-macros rpm-sign yum-utils curl wget zip tar system-rpm-config mlocate -y
-sudo yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.amazonlinux.noarch.rpm -y
+sudo yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.amazonlinux.x86_64.rpm -y
 sudo reboot
